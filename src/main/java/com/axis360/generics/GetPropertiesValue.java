3b94c0c598a0c2fa3469aa2c9d21da94a6048459
yadav_deepak <ydeepkcs@gmail.com>
2019-01-20 00:50:11 +0530

    GetPropertiesValue class created with static String getPropertyValue method.

--- src/main/java/com/axis360/generics/GetPropertiesValue.java
+++ src/main/java/com/axis360/generics/GetPropertiesValue.java
@@ -1,4 +1,36 @@
 package com.axis360.generics;
 
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class GetPropertiesValue {
+
+    // config properties path
+    public static String filePath = "./config.properties";
+
+    // getPropertyValue
+    public static String getPropertyValue(String key) {
+
+        // instantiate Properties class(Java.Util)
+        Properties properties = new Properties();
+
+        try {
+            // instantiate File class and pass filePath
+            File file = new File(filePath);
+            // instantiate FileInputStream class and pass file
+            FileInputStream fileInput = new FileInputStream(file);
+            // Correct comment needed !
+            properties.load(fileInput);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // returns the value that matches the key
+        return properties.getProperty(key);
+    }
 }
